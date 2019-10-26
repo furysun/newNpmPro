a722d37c6462e4ec838197b8b576a31e115f878e
furysun <fury.sun.main@gmail.com>
2019-10-26 13:43:38 +0300

    +

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './js/index.js',
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: 'index.html'
+        })
+    ]
+    ,
+    devServer: {
+        contentBase: path.join(__dirname, '.'),
+        port: 4200
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+};
